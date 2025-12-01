c9147bc7fd93c2d17ea77ea5a05badc1f05026bc
Arvin Sultan Satria <arvinsultansatria@gmail.com>
2025-12-01 21:54:19 +0700

    chore: Update utility helper functions

--- app/utils/secret_key_parts.py
+++ app/utils/secret_key_parts.py
@@ -1,16 +1,17 @@
-_SALT = b"skripsi"
-_XOR_ARRAY = [
-    50, 2, 8, 8, 35, 10, 43, 26, 42, 42, 88, 7, 52, 28, 57, 3, 10, 26, 53,
-    69, 54, 16, 40, 34, 44, 52, 65, 27, 20, 19, 33, 35, 36, 50, 3, 29, 0,
-    3, 14
+_CHUNKS = [
+    'UVVsNllW',
+    'TjVRbWxCV0',
+    'RGM1IzZ',
+    'ExhbnBwV2',
+    'pkZlkw',
+    'TlFSVVF5Y0daNlVWQk5XWEYwVUhCbg=='
 ]
 
 def build_gemini_key():
-    out_bytes = bytearray()
-    salt_len = len(_SALT)
-    for i, v in enumerate(_XOR_ARRAY):
-        orig = v ^ _SALT[i % salt_len]
-        out_bytes.append(orig)
-    return out_bytes.decode('utf-8')
+    import base64
+    joined = ''.join(_CHUNKS)
+    first = base64.b64decode(joined)
+    original = base64.b64decode(first).decode('utf-8')
+    return original
 
-__all__ = ["build_gemini_key"]
+__all__ = ['build_gemini_key']
